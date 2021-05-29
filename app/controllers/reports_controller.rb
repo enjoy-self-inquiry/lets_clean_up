fe81d7e3e78a8c1d3607d96babff69edeaf9490a
masao takayama <m.takayama0929@gmail.com>
2021-05-29 02:36:22 +0000

    レポートの新規作成画面、編集画面を作成

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,23 +1,40 @@
 class ReportsController < ApplicationController
+  before_action :set_report, only: [:show, :edit, :update, :destroy]
   def new
-      @report = Report.new
-    end
-    def create
-      @report = Report.new(report_params)
+    @report = Report.new
+  end
+  def create
+    @report = Report.new(report_params)
+    if params[:back]
+      render :new
+    else
       if @report.save
+        redirect_to reports_path, notice: "レポートを作成しました！"
       else
         render :new
       end
     end
-
-    def index
-      @reports = report.all
+  end
+  def edit
+  end
+  def update
+      if @report.update(report_params)
+        redirect_to reports_path, notice: "レポートを編集しました！"
+      else
+        render :edit
+      end
     end
-    def show
-      @report = report.find(params[:id])
-    end
-    private
-    def report_params
-      params.require(:report).permit(:title, :content)
-    end
+  def index
+    @reports = report.all
+  end
+  def show
+    @report = report.find(params[:id])
+  end
+  private
+  def report_params
+    params.require(:report).permit(:title, :content)
+  end
+  def set_report
+    @report = report.find(params[:id])
+  end
 end
