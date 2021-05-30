647ee2a7dc652607c1e053575133afac251bba5f
masao takayama <m.takayama0929@gmail.com>
2021-05-30 11:33:49 +0000

    レポートを削除できるようにする

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,10 @@
 class ReportsController < ApplicationController
   before_action :set_report, only: [:show, :edit, :update, :destroy]
+
   def new
     @report = Report.new
   end
+
   def create
     @report = Report.new(report_params)
     if params[:back]
@@ -15,25 +17,38 @@ class ReportsController < ApplicationController
       end
     end
   end
+
   def edit
   end
+
   def update
-      if @report.update(report_params)
-        redirect_to reports_path, notice: "レポートを編集しました！"
-      else
-        render :edit
-      end
+    if @report.update(report_params)
+      redirect_to reports_path, notice: "レポートを編集しました！"
+    else
+      render :edit
     end
+  end
+
   def index
     @reports = report.all
   end
+
   def show
     @report = report.find(params[:id])
   end
+
+  def destroy
+    @report.destroy
+    redirect_to reports_path, notice: "レポートを削除しました！"
+  end
+
+
   private
+
   def report_params
     params.require(:report).permit(:title, :content)
   end
+
   def set_report
     @report = report.find(params[:id])
   end
